5c8e59bead802e1fa009fdc4a8fcb877128d05f7
Simon Galasso <sgalasso42@student.42.fr>
2019-10-05 22:47:48 +0200

    wip

--- Blank/Assets/Scripts/FootMarker.cs
+++ Blank/Assets/Scripts/FootMarker.cs
@@ -5,14 +5,31 @@ using UnityEngine;
 public class FootMarker : MonoBehaviour
 {
     public GameObject footMarker;
+    public GameObject footPosition;
+    public float distanceBetweenFootPrint;
+    public float groundCheck;
+
+    private Vector2 lastPos;
 
     private void Start()
     {
-        
+        lastPos = transform.position;
     }
 
-    private void Update()
+    private void FixedUpdate()
     {
-        
+        RaycastHit hit;
+
+        Debug.DrawRay(footPosition.transform.position, -footPosition.transform.up * groundCheck, Color.red);
+
+        if (Vector2.Distance(transform.position, lastPos) > distanceBetweenFootPrint)
+        {
+            if (Physics.Raycast(footPosition.transform.position, -footPosition.transform.up, out hit, groundCheck))
+            {
+                GameObject newDecal = Instantiate(footMarker, hit.point, Quaternion.LookRotation(Vector3.up, hit.normal));
+                newDecal.transform.position -= newDecal.transform.forward * 0.1f;
+                lastPos = transform.position;
+            }
+        }
     }
 }
