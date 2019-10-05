fa7261ec47b16c3cb1752728a98d80d73b919a8a
Alexis Bechet <fervac01@gmail.com>
2019-10-05 23:44:42 +0200

    Added first map, prefabs : Hole, Spikes, Endlevel

---
+++ Blank/Assets/Scripts/Spikes.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spikes : MonoBehaviour
+{
+      public GameObject Player;
+      public GameObject Respawn;
+
+      void OnTriggerEnter(Collider collision)
+      {
+           if(collision.gameObject == Player)
+           {
+                // Red screen, delay d'une seconde
+                Player.transform.position = Respawn.transform.position;
+           }
+      }
+}
---
+++ Blank/Assets/Scripts/endlevel.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class endlevel : MonoBehaviour
+{
+
+    public GameObject Player;
+
+    void OnTriggerEnter(Collider collision)
+    {
+         if(collision.gameObject == Player)
+         {
+              // Pause Game, victory screen time ?
+              SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+         }
+    }
+}
