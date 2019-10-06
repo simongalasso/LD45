0245c3a567713dd10b7833eee22827a4d9c69e00
Alexis Bechet <fervac01@gmail.com>
2019-10-06 17:51:11 +0200

    Spikes anim

--- Blank/Assets/Scripts/Spikes.cs
+++ Blank/Assets/Scripts/Spikes.cs
@@ -6,6 +6,28 @@ public class Spikes : MonoBehaviour
 {
       public GameObject Player;
       public GameObject Respawn;
+      public float time = 3;
+      private bool check = false;
+
+      public IEnumerator spikepop()
+      {
+          check = true;
+          yield return new WaitForSeconds(time);
+          transform.position = transform.position + new Vector3(10, 0, 0);
+          yield return new WaitForSeconds(time);
+          transform.position = transform.position + new Vector3(-10, 0, 0);
+          check = false;
+      }
+
+
+      void Update()
+      {
+          if (check == false)
+          {
+            check = true;
+            StartCoroutine(spikepop());
+          }
+      }
 
       void OnTriggerEnter(Collider collision)
       {
