cf1f1281dbfa34793854fefd5c9db85980274e40
Simon Galasso <sgalasso42@student.42.fr>
2019-10-06 14:11:00 +0200

    [SG] fixes footprints + sounds

--- Blank/Assets/Scripts/FootMarker.cs
+++ Blank/Assets/Scripts/FootMarker.cs
@@ -4,17 +4,22 @@ using UnityEngine;
 
 public class FootMarker : MonoBehaviour
 {
-    public GameObject footMarker;
+    public GameObject footLMarker;
+    public GameObject footRMarker;
     public GameObject footPosition;
     public float distanceBetweenFootPrint;
     public float groundCheck;
     public float footPrintShift;
-
+    
+    private GameObject footMarker;
+    private AudioSource footSound;
     private Vector3 lastPos;
     private int leftFoot = 1;
 
     private void Start()
     {
+        footMarker = footLMarker;
+        footSound = GetComponent<AudioSource>();
         lastPos = transform.position;
     }
 
@@ -28,9 +33,15 @@ public class FootMarker : MonoBehaviour
         {
             if (Physics.Raycast(footPosition.transform.position, -footPosition.transform.up, out hit, groundCheck))
             {
+                footSound.PlayOneShot(footSound.clip);
+                if (leftFoot == 1)
+                    footMarker = footLMarker;
+                else
+                    footMarker = footRMarker;
                 leftFoot = -leftFoot;
-                GameObject newDecal = Instantiate(footMarker, new Vector3(hit.point.x, hit.point.y - 0.1f, hit.point.z), Quaternion.LookRotation(Vector3.up, hit.normal));
-                newDecal.transform.Translate(transform.up * footPrintShift * leftFoot);
+                GameObject newDecal = Instantiate(footMarker, new Vector3(hit.point.x, hit.point.y + 0.1f, hit.point.z), Quaternion.LookRotation(-Vector3.up, hit.normal));
+                newDecal.transform.eulerAngles = new Vector3(newDecal.transform.eulerAngles.x, transform.eulerAngles.y, newDecal.transform.eulerAngles.z);
+                newDecal.transform.position += transform.TransformDirection(Vector3.right * leftFoot) * footPrintShift;
                 lastPos = transform.position;
             }
         }
