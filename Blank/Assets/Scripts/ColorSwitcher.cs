a5892bc99432bb1194030d9fbde7590487a18efe
Simon Galasso <sgalasso42@student.42.fr>
2019-10-06 16:56:35 +0200

    [SG] ColorSwitch script

---
+++ Blank/Assets/Scripts/ColorSwitcher.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorSwitcher : MonoBehaviour
+{
+    public Material materialOn;
+    public Material materialOff;
+
+    private bool state = false;
+
+    public void ChangeColor()
+    {
+        if (state == false)
+            GetComponent<Renderer>().material = materialOn;
+        else
+            GetComponent<Renderer>().material = materialOff;
+        state = !state;
+    }
+}
