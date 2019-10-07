4da2c1f5ef08db1e07c554a970c80ffbfe0b2f17
Simon Galasso <sgalasso42@student.42.fr>
2019-10-07 17:59:19 +0200

    Debug and particles

--- Blank/Assets/Scripts/endlevel.cs
+++ Blank/Assets/Scripts/endlevel.cs
@@ -5,35 +5,14 @@ using UnityEngine.SceneManagement;
 
 public class endlevel : MonoBehaviour
 {
-
     public GameObject Player;
     public float range = 10f;
-    private Renderer rend;
-
-    void Start()
-    {
-        rend = GetComponent<Renderer>();
-        rend.enabled = false;
-    }
-
-    void Update()
-    {
-        if(Vector3.Distance(Player.transform.position, transform.position) <= range)
-        {
-          rend.enabled = true;
-        }
-        else
-        {
-          rend.enabled = false;
-        }
-    }
 
     void OnTriggerEnter(Collider collision)
     {
-         if(collision.gameObject == Player)
+         if (collision.gameObject == Player)
          {
-              // Pause Game, victory screen time ?
-              SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+              SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
          }
     }
 }
